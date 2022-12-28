95a79f057dd3947936dc4af8caedcceb98f5d74e
markixa <laiamr77@gmail.com>
2022-12-28 17:05:13 +0100

    first commit :atom: :leaves:

---
+++ src/main/java/com/ironhack/laiatrailcat/LaiaTrailcatApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.laiatrailcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaiaTrailcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaiaTrailcatApplication.class, args);
+    }
+
+}
